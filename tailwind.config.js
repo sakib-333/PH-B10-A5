23107d66fb6e75147557271502475d8029135e31
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-09-23 12:28:04 +0600

    Add navbar.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const color = require("./node_modules/tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
@@ -5,6 +7,11 @@ module.exports = {
     extend: {},
     fontFamily: {
       lexend: ["Lexend Deca", "sans-serif"],
+    },
+    colors: {
+      ...color,
+      "donate-100": "#faf8f5",
+      "donate-200": "#b5f562",
     },
   },
   plugins: [require("daisyui")],
