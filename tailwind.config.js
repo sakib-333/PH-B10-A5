205ec97a65e3b4168ab903db403d73cf25aebd6e
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-09-23 11:16:55 +0600

    Add lexend font style.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      lexend: ["Lexend Deca", "sans-serif"],
+    },
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [require("daisyui")],
 };
