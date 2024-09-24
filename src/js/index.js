fd42bb90e87323a8f0e7fdae97b9e2801c77a3fb
Sakibur Rahman <sakib.cse.333@gmail.com>
2024-09-24 08:05:38 +0600

    Add history section.

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,52 @@
+const yourCurrentBalanceField = document.getElementById("your-current-balance");
+const donationHistroryDiv = document.getElementById("donation-history-div");
+
+let showSectors = true;
+
 document.getElementById("visit-blog").addEventListener("click", () => {
   window.location.href = "./src/pages/blog.html";
-});
-
-document.getElementById("btn-donation").addEventListener("click", () => {
-  showActiveButton("btn-donation");
-});
-
-document.getElementById("btn-history").addEventListener("click", () => {
-  showActiveButton("btn-history");
 });
 
 const markups = sectors.map((sector) => {
   return makeSectorCard(sector);
 });
 
-const main = document.querySelector("main");
+const donationSectors = document.querySelector("#donation-sectors-div");
 
 for (let markup of markups) {
   const element = createElement("div");
   element.innerHTML = markup;
 
-  main.appendChild(element);
+  donationSectors.appendChild(element);
 }
+
+for (let sector of sectors) {
+  document
+    .getElementById(`sector-form-${sector.id}`)
+    .addEventListener("submit", (e) => {
+      e.preventDefault();
+      const givenDonation = document.getElementById(
+        `sector-input-${sector.id}`
+      ).value;
+      if (checkValidDonation(givenDonation)) {
+        increaseSectorDonation(
+          `sector-income-${sector.id}`,
+          Number(givenDonation)
+        );
+        yourCurrentBalanceField.innerText =
+          Number(yourCurrentBalanceField.innerText) - givenDonation;
+        document.getElementById(`sector-input-${sector.id}`).value = "";
+        createDonationHistory(donationHistroryDiv, sector, givenDonation);
+      } else {
+        window.alert("Invalid donation!");
+      }
+    });
+}
+
+document.getElementById("btn-donation").addEventListener("click", () => {
+  showActiveButton("btn-donation", "donation-sectors-div");
+});
+
+document.getElementById("btn-history").addEventListener("click", () => {
+  showActiveButton("btn-history", "donation-history-div");
+});
--- src/js/utility.js
+++ src/js/utility.js
@@ -1,17 +1,46 @@
-function showActiveButton(id) {
+function showActiveButton(id1, id2) {
   document.getElementById("btn-donation").classList.remove("bg-donate-200");
   document.getElementById("btn-history").classList.remove("bg-donate-200");
 
-  document.getElementById(id).classList.add("bg-donate-200");
+  document.getElementById("donation-sectors-div").classList.add("hidden");
+  document.getElementById("donation-history-div").classList.add("hidden");
+
+  document.getElementById(id1).classList.add("bg-donate-200");
+  document.getElementById(id2).classList.remove("hidden");
 }
 
 function createElement(element) {
   return document.createElement(element);
 }
 
+function checkValidDonation(donation) {
+  const amount = Number(donation);
+  return typeof amount !== NaN && amount >= 0;
+}
+
+function increaseSectorDonation(id, donation) {
+  const income = Number(document.getElementById(id).innerText) + donation;
+  document.getElementById(id).innerText = income;
+}
+
+function createDonationHistory(donationHistroryDiv, sector, givenDonation) {
+  const div = document.createElement("div");
+  div.innerHTML = `
+      <div class="p-4 border rounded-md">
+        <h1 class="text-xl font-bold">
+          ${givenDonation} Taka is Donated for ${sector.status}
+        </h1>
+        <p>
+          Date : ${new Date()}
+        </p>
+      </div>
+  `;
+  donationHistroryDiv.appendChild(div);
+}
+
 function makeSectorCard(sector) {
   return `
-        <div class="card lg:card-side bg-base-100 border" key="${sector.id}">
+        <div class="card lg:card-side bg-base-100 border" >
           <figure class="p-8">
             <img class="w-full" src="${sector.url}" alt="Album" />
           </figure>
@@ -20,14 +49,15 @@ function makeSectorCard(sector) {
               class="flex items-center space-x-2 bg-zinc-100 p-2 w-40 rounded"
             >
               <img src="./src/assets/coin.png" alt="coin" />
-              <h1><span>${sector.income}</span> BDT</h1>
+              <h1><span id=sector-income-${sector.id}>${sector.income}</span> BDT</h1>
             </div>
             <h2 class="card-title">${sector.headding}</h2>
             <p class="text-justify">${sector.text}</p>
             <div>
-              <form>
+              <form id=sector-form-${sector.id}>
                 <div class="form-control">
                   <input
+                    id=sector-input-${sector.id}
                     type="text"
                     placeholder="Write Donation Amount"
                     class="input input-bordered"
